786d1b97e2f2c1aab042d6c8bc827987df5abc7c
Leontian36 <62909257+Leontian36@users.noreply.github.com>
2020-06-04 16:20:47 -0400

    update

--- run.py
+++ run.py
@@ -1,9 +1,9 @@
 import subprocess
 from datetime import date, timedelta
 
-subprocess.run(["python", "json2csv.py"])
+subprocess.run(["python", "Denmark-crawler.py"])
 
 for d in range(1, 4):
     day = date.today() - timedelta(days=d)
     day = day.strftime('%Y-%m-%d')
-    subprocess.run(["python", "json2csv.py", day])
+    subprocess.run(["python", "Denmark-crawler.py", day])
