58f6a16f03ac20f6d4b21657fa39e830694edc0a
stevenliuyi <stevenliuyi@gmail.com>
2020-06-04 17:42:14 -0400

    fix

--- run.py
+++
@@ -1,9 +0,0 @@
-import subprocess
-from datetime import date, timedelta
-
-subprocess.run(["python", "Denmark-crawler.py"])
-
-for d in range(1, 4):
-    day = date.today() - timedelta(days=d)
-    day = day.strftime('%Y-%m-%d')
-    subprocess.run(["python", "Denmark-crawler.py", day])
