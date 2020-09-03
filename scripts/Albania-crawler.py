04a6b95c478c5afca5222e60e7fb8afec8dba814
Leontian36 <62909257+Leontian36@users.noreply.github.com>
2020-09-03 18:04:41 -0400

    update

--- scripts/Albania-crawler.py
+++ scripts/Albania-crawler.py
@@ -1,7 +1,7 @@
 '''
 @Author: Matthew Shabet
-@Date: 2020-09-03 21:51:00
-@LastEditTime: 2020-09-03 22:48:00
+@Date: 2020-08-02 21:51:00
+@LastEditTime: 2020-08-02 22:48:00
 @LastEditors: Please set LastEditors
 @Description: In User Settings Edit
 '''
@@ -11,7 +11,7 @@ import json
 import os
 from datetime import datetime
 
-url = 'https://www.mohfw.gov.in/data/datanew.json'
+url = 'https://coronavirus.al/api/qarqet.php'
 
 # Get the data
 response = requests.get(url, headers={'Connection': 'close'})
@@ -19,15 +19,14 @@ data = json.loads(response.text)
 
 # Create and open the CSV
 mkfile_time = datetime.strftime(datetime.now(), '%Y%m%d%H%M')
-folder_path = './photo/India/'+ mkfile_time + '/'
+folder_path = './photo/Albania/'+ mkfile_time + '/'
 if not os.path.exists(folder_path):
     os.makedirs(folder_path)
 file = open(folder_path+'table.csv', 'w', newline='', encoding='utf-8-sig')
 writer = csv.writer(file)
 
-# Write each line to the CSV
-headers = ["state_name", "active", "positive", "cured", "death", "new_active", "new_positive", "new_cured", "new_death"]
+headers = ["Region", "Total cases", "Recovered", "Deaths", "Active", "Intensive care", "Hospital treatment", "Home isolated", "Total tests"]
 writer.writerow(headers)
 for d in data:
-  row = [d[h] for h in headers]
+  row = [d["qarku"], d["raste_gjithsej"], d["sheruar"], d["vdekur"], d["akt_pozitive"], d["terapi_int"], d["mjekim_spitalor"], d["izolim"], d["teste"]]
   writer.writerow(row)
